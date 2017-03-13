6f1531a0a790c1950daa9ada89dfdd093ca99118
KatieAJenkins <kind2karma@gmail.com>
2017-03-12 19:06:57 -0600

    added mocha test js script to package json

---
+++ twoSums.js
@@ -0,0 +1,28 @@
+'use strict';
+// Two Sum: Write a function that takes an array of integers and a target integer.
+
+//It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2].
+
+// input: [1, 2, 3, 4, 5, 6, 7], 13 // output: [5,6]
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+let target = 13;
+
+function twoSums (array, target){
+  
+  for(var i = 0; i < array.length; i++) {
+  console.log(array[i]);
+  console.log(array[i+1]);
+  if (array[i] + array[i+1] === target) {
+    return array[i] && array[i+1];
+    return [];
+  }
+  else {
+    return false;
+  }
+}
+}
+
+twoSums(array, target);
+
+module.exports = twoSums;
